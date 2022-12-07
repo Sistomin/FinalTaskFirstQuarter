40e27825cef72a9b1bf9f5908d865ea8199ca713
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-12-07 09:38:34 +0700

    add comments

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <=3 символам
 
-string[] ArrTask;
-Console.WriteLine("Введите массив строк через запятую (без пробелов):");
-string stroka = Console.ReadLine();
-ArrTask = stroka.Split(',');
+string[] ArrTask; //объявляем массив
+Console.WriteLine("Введите массив строк через запятую (без пробелов):"); // сообщение пользователю программы
+string stroka = Console.ReadLine(); //принимаем введенное значение пользователем с клавиатуры, присваеваем переменной stroka
+ArrTask = stroka.Split(','); // разбиваем массив на подстроки при помощи .Split, разделителем используем ','
 var result = new string[ArrTask.Length];
 var realSize = 0;
 foreach (var value in ArrTask)
